b7f1f8e11c260db67c0768c40956be9f8ea63fbb
Christhian David Santander Figueroa <ec2-user@ip-172-31-27-178.us-east-2.compute.internal>
2018-04-07 15:32:14 +0000

    ver 0.1 Autenticacion con roles

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', 'AdminController@admin')->middleware('role:false')
+    ->name('admin');
+    
+Route::get('/overview', 'AdminController@overview')->middleware('role:true')
+    ->name('overview');
+    
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
